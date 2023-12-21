d1e458fc45bdf8a3ffd148e25f142e8ac9e6bda9
wes0326 <wes0326@gmail.com]>
2023-12-21 14:18:44 +0800

    done

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
-const input = document.querySelector(".input");
-const buttons = document.querySelectorAll("button");
-const specialChars = ["%","*","/","+","-","="];
-let output = "";
 
-
-const calculate = (btnValue) => {
-    if (btnValue === "=" && btnValue !== "") {
-        output = eval(output.replace("%", "/100"));
-    } else if (btnValue === "AC") {
+    const input = document.getElementById("num");
+    const operatorButtons = document.querySelectorAll(".operator");
+    const numberButtons = document.querySelectorAll(".buttons button:not(.operator)");
+    const specialChars = ["%", "*", "/", "+", "-", "="];
+    let output = "";
+  
+    const calculate = (btnValue) => {
+      if (btnValue === "=" && btnValue !== "") {
+        // Perform percentage calculation if the operator is "%"
+        if (output.includes("%")) {
+          output = eval(output.replace("%", "/100"));
+        } else {
+          output = eval(output);
+        }
+      } else if (btnValue === "AC") {
         output = "";
-    } else if (btnValue === "Del") {
+      } else if (btnValue === "Del") {
         output = output.toString().slice(0, -1);
-    } else {
+      } else {
         if (output === "" && specialChars.includes(btnValue)) {
-            return;
+          return;
         }
         output += btnValue;
-    }
-    input.value = output;
-};
-
-
-// Add event listener to buttons, call calculate() on click
-buttons.forEach(function(button) {
-    button.addEventListener("click", function(e) {
+      }
+      input.value = output;
+    };
+  
+    // Add event listener to operator buttons, call calculate() on click
+    operatorButtons.forEach(function (button) {
+      button.addEventListener("click", function (e) {
         calculate(e.target.dataset.value);
+      });
     });
-});
+  
+    // Add event listener to number buttons, append value to output on click
+    numberButtons.forEach(function (button) {
+      button.addEventListener("click", function (e) {
+        const btnValue = e.target.dataset.value;
+        output += btnValue;
+        input.value = output;
+      });
+    });
+
 
